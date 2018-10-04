5e2148153e34284fe65a3efaf414104013c3504b
KasperHoulind <32668404+KasperHoulind@users.noreply.github.com>
2018-10-04 19:27:08 +0200

    DLL TIl Mandatory
    Methods for converting grams to oz and oz to grams

---
+++ TestDLL/Convert1Dll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDLL
+{
+    public class Convert1Dll
+    {
+        private const double Oz = 28.3495;
+        private const double Grams = 0.0353;
+
+
+        public static double OzToGrams(double ounces)
+        {
+            //Converts Ounces to Grams by multiplying with 28.3495
+            return ounces * Oz;
+        }
+
+
+        public static double GramsToOz(double grams)
+        {
+            //Converts grams to Ounces by multiplying with 0.0353
+            return grams * Grams;
+        }
+    }
+}
